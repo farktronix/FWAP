9df010717ae9efdfc1d37361f45c0349fb42ca4d
Jacob Farkas <jacobf@rkas.net>
2019-12-27 23:45:54 -0800

    Initial commit. Currently this just fetches the current sea level atmospheric pressure to feed into the MPL3115A2 chip

---
+++ NOAA.py
@@ -0,0 +1,71 @@
+#!/usr/local/bin/python3
+
+import requests
+
+class NOAA:
+    NOAA_BASE_URL="https://api.weather.gov"
+    DEBUG=True
+
+    def __init__(self, latLon=None):
+        self._latLon = latLon
+        self._stationID = None
+
+    def debugLog(self, msg):
+        if self.DEBUG:
+            print(msg)
+
+    def doRequest(self, url):
+        self.debugLog("📜  Sending request to %s" % url)
+        req = requests.get(url)
+        if req.status_code != 200:
+            raise  Exception("Error: {} {}".format(req.status_code, req.reason))
+        return req.json()
+
+    @property
+    def latLon(self):
+        if self._latLon is None:
+            self.debugLog("🌎  Fetching lat/long from geoapi...")
+            req = self.doRequest("https://freegeoip.app/json/")
+            self._latLon = [req["latitude"], req["longitude"]]
+
+        return self._latLon
+
+    @property
+    def latLonString(self):
+        return ",".join(map(str,self.latLon))
+
+
+    @property
+    def nearestStationID(self):
+        if self._stationID is None:
+            stations = self.getStations()
+            if stations is not None and len(stations) > 0:
+                station = stations[0]
+                stationID = station['properties']['stationIdentifier']
+                self.debugLog("📡  Nearest station is %s" % stationID)
+                self._stationID = stationID
+        return self._stationID
+
+
+
+    """ Expects latitude and longitude as a comma separated string
+    """
+    def getStations(self):
+        requestURL = self.NOAA_BASE_URL+"/points/"+self.latLonString+"/stations"
+        self.debugLog("🌨  Fetching stations from NOAA...")
+        stationsRequest = self.doRequest(requestURL)
+        return stationsRequest["features"]
+
+    def getNearestLatestObservation(self):
+        requestURL = self.NOAA_BASE_URL+"/stations/{}/observations/latest".format(self.nearestStationID)
+        observation = self.doRequest(requestURL)
+        return observation
+
+    def getCurrentSeaLevelPressure(self):
+        obs = self.getNearestLatestObservation()
+        pressure = obs['properties']['seaLevelPressure']['value']
+        units = obs['properties']['seaLevelPressure']['unitCode']
+        if units == "unit:Pa":
+            return pressure
+        else:
+            raise Exception("Error: Unknown pressure units (%s)" % units)
---
+++ README.md
@@ -0,0 +1,6 @@
+# FWAP
+## Farkas Weather Analysis for raspberry Pi
+
+This code fetches data from [MPL3115A2](datasheets/MPL3115A2.pdf) and [Si7021](datasheets/Si7021.pdf) breakout boards, both built by Adafruit.
+
+This is still a work in progress, but that data will eventually be fed into a display of current weather data also driven by the same script running on a Raspberry Pi.
